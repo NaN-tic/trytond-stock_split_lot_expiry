68719361a1b3fd0315a29829e1374d762c31c149
?ngel ?lvarez <angel@nan-tic.com>
2013-10-20 11:45:11 +0200

    remove split from supplier shipments

--- stock.py
+++ stock.py
@@ -2,8 +2,8 @@
 # copyright notices and license terms.
 from datetime import date
 
-from trytond.model import ModelView
-from trytond.pyson import Equal, Eval, Not
+from trytond.model import ModelView, fields
+from trytond.pyson import Eval, Not
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 
@@ -14,34 +14,42 @@ __metaclass__ = PoolMeta
 class Move:
     __name__ = 'stock.move'
 
+    allow_split_lot_expiry = fields.Function(fields.Boolean('Allow Split'),
+        'get_allow_split_lot_expiry')
+
     @classmethod
     def __setup__(cls):
         super(Move, cls).__setup__()
         cls._error_messages.update({
-            'invalid_split_by_expiry_date': 'You are trying to split the '
+            'invalid_split_by_lot_expiry': 'You are trying to split the '
                 'Stock Move "%s" by Expiry Dates but it is in a invalid state '
                 '(Assigned, Done or Cancelled) or it already has a Lot.',
             })
         cls._buttons.update({
-                'split_by_expiry_date': {
-                    'invisible': Eval('state').in_(
-                        ['cancel', 'assigned' 'done']),
+                'split_by_lot_expiry': {
+                    'invisible': Not(Eval('allow_split_lot_expiry', False)),
                     },
                 })
 
+    def get_allow_split_lot_expiry(self, name):
+        return (self.state not in ('cancel', 'assigned', 'done') and
+            not self.lot
+            and (not self.shipment
+                or self.shipment.__name__ != 'stock.shipment.in'))
+
     @classmethod
     @ModelView.button
-    def split_by_expiry_date(cls, moves):
+    def split_by_lot_expiry(cls, moves):
         for move in moves:
-            move._split_by_expiry_date()
+            move._split_by_lot_expiry()
 
-    def _split_by_expiry_date(self):
+    def _split_by_lot_expiry(self):
         pool = Pool()
         Lot = pool.get('stock.lot')
         Uom = pool.get('product.uom')
 
-        if self.state in ('done', 'cancel', 'assigned') or self.lot:
-            self.raise_user_error('invalid_split_by_expiry_date',
+        if not self.allow_split_lot_expiry:
+            self.raise_user_error('invalid_split_by_lot_expiry',
                 (self.rec_name,))
 
         date_end = self.effective_date or self.planned_date or date.today()
@@ -60,9 +68,8 @@ class Move:
             for lot in lots:
                 lots_and_qty.append((lot, lot.quantity))
 
-        moves = [self]
         if not lots_and_qty:
-            return moves
+            return [self]
 
         state = self.state
         self.write([self], {
@@ -72,23 +79,22 @@ class Move:
         remainder = self.internal_quantity
         current_lot, current_lot_qty = lots_and_qty.pop(0)
         if current_lot_qty >= remainder:
-            quantity = Uom.compute_qty(self.product.default_uom, remainder,
-                self.uom)
-            self.write(moves, {
+            self.write([self], {
                     'lot': current_lot.id,
-                    'quantity': quantity,
+                    'quantity': Uom.compute_qty(self.product.default_uom,
+                        remainder, self.uom),
                     'state': state,
                     })
-            return moves
+            return [self]
 
-        quantity = Uom.compute_qty(self.product.default_uom,
-            current_lot_qty, self.uom)
-        self.write(moves, {
+        self.write([self], {
                 'lot': current_lot.id,
-                'quantity': quantity,
+                'quantity': Uom.compute_qty(self.product.default_uom,
+                    current_lot_qty, self.uom),
                 })
         remainder -= current_lot_qty
 
+        moves = [self]
         while remainder > 0.0 and lots_and_qty:
             current_lot, current_lot_qty = lots_and_qty.pop(0)
             quantity = min(current_lot_qty, remainder)
@@ -100,7 +106,7 @@ class Move:
             remainder -= quantity
         if remainder > 0.0:
             moves.extend(self.copy([self], {
-                        'lot': current_lot.id,
+                        'lot': None,
                         'quantity': Uom.compute_qty(self.product.default_uom,
                             remainder, self.uom),
                         }))
@@ -116,19 +122,16 @@ class ShipmentOut:
     __name__ = 'stock.shipment.out'
 
     @classmethod
-    def __setup__(cls):
-        super(ShipmentOut, cls).__setup__()
-        cls._buttons.update({
-                'split_moves_by_expiry_date': {
-                    'invisible': Not(Equal(Eval('state', ''), 'waiting')),
-                    },
-                })
-
-    @classmethod
     @ModelView.button
-    def split_moves_by_expiry_date(cls, shipments):
+    def assign_try(cls, shipments):
         for shipment in shipments:
             for move in shipment.inventory_moves:
-                if (not move.lot and
-                        move.state not in ('cancel', 'assigned', 'done')):
-                    move._split_by_expiry_date()
+                lot_required = ('customer'
+                        in [t.code for t in move.product.lot_required]
+                    or move.product.lot_is_required(move.from_location,
+                        move.to_location))
+                if move.allow_split_lot_expiry and lot_required:
+                    splitted_moves = move._split_by_lot_expiry()
+                    if not all(bool(m.lot) for m in splitted_moves):
+                        return False
+        return super(ShipmentOut, cls).assign_try(shipments)
