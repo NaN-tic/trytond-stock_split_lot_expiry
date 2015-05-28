754314911a50cda46790696d54b6e2813cffa175
Guillem Barba <guillem@nan-tic.com>
2015-05-28 12:19:58 +0200

    Don't failg try_assign if split is not done

--- stock.py
+++ stock.py
@@ -137,9 +137,5 @@ class ShipmentOut:
                     or move.product.lot_is_required(move.from_location,
                         move.to_location))
                 if move.allow_split_lot_expiry and lot_required:
-                    splitted_moves = move._split_by_lot_expiry()
-                    if not all(bool(m.lot) for m in splitted_moves):
-                        assigned = False
-        if not assigned:
-            return False
+                    move._split_by_lot_expiry()
         return super(ShipmentOut, cls).assign_try(shipments)
