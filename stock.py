3cea64cc58f38d950fc40052a30d99522052fad6
Sim? Albert i Beltran <sim6@probeta.net>
2018-10-23 13:48:44 +0200

    Consider quantity of all production inputs #035375

--- stock.py
+++ stock.py
@@ -1,7 +1,5 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from datetime import date
-
 from trytond.model import ModelView, fields
 from trytond.pyson import Eval, Not
 from trytond.pool import Pool, PoolMeta
@@ -108,6 +106,19 @@ class Move:
             for move in moves:
                 if move.product not in lots_and_qty:
                     continue
+                if hasattr(move, 'production_input'):
+                    for production_input in move.production_input.inputs:
+                        if (production_input.product == move.product
+                                and production_input.state == 'draft'
+                                and production_input.lot):
+                            new_lots_and_qty = []
+                            for lot, quantity in lots_and_qty[move.product]:
+                                if lot == production_input.lot:
+                                    quantity -= production_input.quantity
+                                    if quantity <= 0:
+                                        continue
+                                new_lots_and_qty.append((lot, quantity))
+                            lots_and_qty[move.product] = new_lots_and_qty
                 remainder = move.internal_quantity
                 current_lot, current_lot_qty = \
                     lots_and_qty[move.product].pop(0)
